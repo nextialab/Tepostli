b3033f9b743f9701a761fbb1ef7c9c06e6c27078
Nelson Iván González <nigm2005@gmail.com>
2017-09-21 15:57:47 -0500

    Adding sparser

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,39 @@
-extern crate regex;
-
 use std::net::UdpSocket;
-use regex::Regex;
 use std::str;
 
-let response = Regex.new(r"\(init\s+(l|r)\s+(\d+)\s+(\w+)\)").unwrap();
+mod sparser;
+
+fn receive(socket: &UdpSocket) -> String {
+	let mut buf = [0; 4096];
+	match socket.recv_from(&mut buf) {
+		Ok((_, _)) => {
+			match str::from_utf8(&buf) {
+				Ok(message) => {
+					return message.to_string();
+				}
+				Err(_) => return "(error could_not_read)".to_string()
+			}
+		}
+		Err(_) => return "(error could_not_read)".to_string()
+	}
+}
 
 fn main() {
     println!("Tepostli 0.1.0");
     match UdpSocket::bind("0.0.0.0:0") {
     	Ok(socket) => {
-    		match socket.send_to("(init Tepostli (version 15.1))".as_bytes(), "127.0.0.1:6000") {
-    			Ok(size) => {
-    				println!("Sent {}", size);
+    		match socket.send_to("(init Ocelotl (version 15.1))".as_bytes(), "127.0.0.1:6000") {
+    			Ok(_) => {
     				let mut i = 0;
     				loop {
-	    				let mut buf = [0; 4096];
-	    				match socket.recv_from(&mut buf) {
-	    					Ok((bytes, _)) => {
-	    						match str::from_utf8(&buf) {
-	    							Ok(message) => {
-	    								println!("Received {}", bytes);
-	    								println!("Message {}", message);
-	    							}
-	    							Err(e) => panic!("Could not read string {}", e)
-	    						}
-	    					}
-	    					Err(e) => panic!("Could not receive {}", e)
-	    				}
+	    				let response = receive(&socket);
+						let slist = sparser::sparser(response);
+						if slist.len() > 0 {
+							match slist[0] {
+								sparser::SList::Atom(ref string) => println!("Received {}", string),
+								_ => println!("Expected SList::Atom")
+							}
+						}
 	    				i = i + 1;
 	    				if i > 100 {
 	    					break;
---
+++ src/sparser.rs
@@ -0,0 +1,111 @@
+use std::iter::Peekable;
+use std::str::Chars;
+
+enum Token {
+    LPar,
+    RPar,
+    Item(String),
+    End
+}
+
+pub enum SList {
+    Atom(String),
+    List(Vec<SList>)
+}
+
+fn next(mut from: Peekable<Chars>) -> (Token, Peekable<Chars>) {
+    let mut local = from.clone();
+    while let Some(p) = local.next() {
+        match p {
+            ' ' => {
+                from.next();
+                continue;
+            },
+            '(' => {
+                from.next();
+                return (Token::LPar, from);
+            },
+            ')' => {
+                from.next();
+                return (Token::RPar, from);
+            },
+            _ => {
+                let mut token: Vec<char> = Vec::new();
+                let mut local = from.clone();
+                while let Some(p) = local.next() {
+                    match p {
+                        ' ' | '(' | ')' => break,
+                        _ => {
+                            from.next();
+                            token.push(p);
+                        }
+                    }
+                }
+                if token.len() > 0 {
+                    let string: String = token.into_iter().collect();
+                    return (Token::Item(string), from);
+                } else {
+                    return (Token::End, from);
+                }
+            }
+        }
+    } 
+    return (Token::End, from);
+} 
+
+fn parse(mut chars: Peekable<Chars>) -> (Vec<SList>, Peekable<Chars>) {
+    let mut slist: Vec<SList> = Vec::new();
+    loop {
+        let (token, from) = next(chars);
+        match token {
+            Token::LPar => {
+                let (nestedlist, nestedchars) = parse(from);
+                slist.push(SList::List(nestedlist));
+                chars = nestedchars;
+            },
+            Token::RPar => {
+                chars = from;
+                break;
+            },
+            Token::Item(string) => {
+                chars = from;
+                slist.push(SList::Atom(string));
+            },
+            Token::End => {
+                chars = from;
+                break;
+            },
+        }
+    }
+    return (slist, chars);
+}
+
+pub fn sparser(string: String) -> Vec<SList> {
+    let mut chars = string.chars().peekable();
+    chars.next();
+    let (slist, _) = parse(chars);
+    return slist;
+}
+
+/* Example
+
+fn print(list: Vec<SList>, deep: usize) {
+    for item in list {
+        match item {
+            SList::Atom(string) => {
+                println!("{}{}", std::iter::repeat(" ").take(deep).collect::<String>(), string);
+            },
+            SList::List(slist) => {
+                print(slist, deep + 1);
+            }
+        }
+    }
+}
+
+fn main() {
+    let test = "(a test list with (a nested (a nested (and a nested)) list) and 4 elements (just for fun))";
+    let slist = sparser(test.to_string());
+    print(slist, 0);
+}
+
+*/
